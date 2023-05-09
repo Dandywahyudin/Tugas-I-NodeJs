610bd07f84bff30e82b83fba8bb37400db4fd85b
Dandywahyudin <dandywahyudin19@gmail.com>
2023-05-09 22:10:30 +0700

    Tugas I NodeJs Bootcamp Alkademi Pesilat

---
+++ moduleBiodata.js
@@ -0,0 +1,13 @@
+const biodata = () => {
+    const nama = 'Dandy Wahyudin';
+    const tmpLahir = 'Bandung';
+    const tglLahir = '19 Oktober 2001';
+    const alamat = 'Griya Cempaka arum K5 no.4';
+
+    console.log(`Nama: ${nama}`);
+    console.log(`Tempat Lahir: ${tmpLahir}`);
+    console.log(`Tanggal Lahir: ${tglLahir}`);
+    console.log(`Alamat: ${alamat}`);
+};
+
+biodata();
